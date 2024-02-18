6281a85ab94721127c65f0bd47f6651622896bf5
MSskej <maciejsmolko03@gmail.com>
2024-02-18 16:48:17 +0100

    Wykonany update. Plik działa jak powinien.

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ import re
 
 f = open("strona.html", encoding="utf8")
 
+text = f.read()
 
-x = re.findall(r"\bprice=\"[1-9][0-9]\...?\"", f.read())
 
-print(x)
+ceny = re.findall(r"\bprice=\"([1-9][0-9]\...?)\"", text)
+tytuly = re.findall(r'aria-hidden="true">(.+?)</a>', text)
+
+ksiazki = list(zip(tytuly, ceny))
+
+for tytul, cena in ksiazki:
+    print("Tytuł:", tytul)
+    print("Cena:", cena)
+    print()
 
 f.close()
+
